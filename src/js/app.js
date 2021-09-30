0b39c2cbcad4ad1f4af8d95d53bf28c71208773f
wishnuprogramming <wishnupmi@gmail.com>
2021-09-30 17:30:40 +0700

    first commit

---
+++ src/js/app.js
@@ -0,0 +1,54 @@
+import $ from 'dom7';
+import Framework7 from './framework7-custom.js';
+
+// Import F7 Styles
+import '../css/framework7-custom.less';
+
+// Import Icons and App Custom Styles
+import '../css/icons.css';
+import '../css/app.less';
+
+
+// Import Routes
+import routes from './routes.js';
+// Import Store
+import store from './store.js';
+
+// Import main app component
+import App from '../app.f7';
+
+
+var app = new Framework7({
+  name: 'My App', // App name
+  theme: 'auto', // Automatic theme detection
+  el: '#app', // App root element
+  component: App, // App main component
+
+  // App store
+  store: store,
+  // App routes
+  routes: routes,
+  
+});
+
+// cek();
+
+// function cek(){
+//   var status = localStorage.getItem("status");
+//   if (status=="login") {
+//      // app.dialog.alert("Berhasil Login");
+    
+    
+//     //  $f7.router.navigate('/about/');
+//      $f7.main.router.navigate('/about/');
+//      // remove clear hanya sementara
+//     // localStorage.removeItem("status");
+//     // localstorage.clear();
+//     // views.main.router.navigate('/about/');
+//     // app.views.main.router.navigate('/about/');
+
+//   } else {
+//     // app.views.main.router.navigate('/home/');
+//   }
+// }
+
---
+++ src/js/routes.js
@@ -0,0 +1,150 @@
+
+import HomePage from '../pages/home.f7';
+import AboutPage from '../pages/about.f7';
+import FormPage from '../pages/form.f7';
+import LoginPage from '../pages/login.f7';
+import DashboardPage from '../pages/dashboard.f7';
+
+import DynamicRoutePage from '../pages/dynamic-route.f7';
+import RequestAndLoad from '../pages/request-and-load.f7';
+import NotFoundPage from '../pages/404.f7';
+
+function checkAuth({ to, from, resolve, reject }) {
+  let check = true
+  var router = this;
+  var status = localStorage.getItem("status");
+  if (status=="login") {
+    resolve({
+      component: AboutPage
+    });
+    // alert("sedang login");
+    // router.navigate('/about/');
+  } else {
+    resolve({
+      component: HomePage
+    });
+    // reject();
+    // router.navigate('/about/');
+  }
+}
+function checkPermission({ to, from, resolve, reject }) {
+  let check = true
+  var router = this;
+  var status = localStorage.getItem("status");
+  if (status=="login") {
+    resolve();
+  } else {
+    reject();
+  }
+}
+  
+var routes = [
+  {
+    path: '/',
+    component: HomePage,
+    beforeEnter: [checkAuth],
+    redirect: function ({to, resolve, reject}) {
+      // if we have "user" query parameter
+      var status = localStorage.getItem("status");
+      // alert(status);
+      if (status=="login") {
+        // redirect to such url
+        resolve('/dashboard/?user=' + to.query.user);
+      }
+      // // otherwise do nothing
+      else { 
+        reject();
+        resolve('/login/'); 
+      }
+    }
+  },
+  {
+    path: '/dashboard/',
+    component: DashboardPage,
+    
+    // beforeEnter: [checkAuth],
+    // beforeEnter: [checkAuth, checkPermission],
+  },
+  {
+    path: '/about/',
+    component: AboutPage,
+    // beforeEnter: [checkAuth],
+    // beforeEnter: [checkAuth, checkPermission],
+  },
+  {
+    path: '/login/',
+    component: LoginPage,
+    // beforeEnter: [checkAuth],
+    // beforeEnter: [checkAuth, checkPermission],
+  },
+  {
+    path: '/form/',
+    component: FormPage,
+  },
+  {
+    path: '/dynamic-route/blog/:blogId/post/:postId/',
+    component: DynamicRoutePage,
+  },
+  {
+    path: '/request-and-load/user/:userId/',
+    async: function ({ router, to, resolve }) {
+      // App instance
+      var app = router.app;
+
+      // Show Preloader
+      app.preloader.show();
+
+      // User ID from request
+      var userId = to.params.userId;
+
+      // Simulate Ajax Request
+      setTimeout(function () {
+        // We got user data from request
+        var user = {
+          firstName: 'Vladimir',
+          lastName: 'Kharlampidi',
+          about: 'Hello, i am creator of Framework7! Hope you like it!',
+          links: [
+            {
+              title: 'Framework7 Website',
+              url: 'http://framework7.io',
+            },
+            {
+              title: 'Framework7 Forum',
+              url: 'http://forum.framework7.io',
+            },
+          ]
+        };
+        // Hide Preloader
+        app.preloader.hide();
+
+        // Resolve route to load page
+        resolve(
+          {
+            component: RequestAndLoad,
+          },
+          {
+            props: {
+              user: user,
+            }
+          }
+        );
+      }, 1000);
+    },
+  },
+  {
+    path: '(.*)',
+    component: NotFoundPage,
+  },
+];
+
+
+// cek();
+
+// function cek(){
+//   var router = this;
+//   // reject();
+//   router.navigate('/about/');
+// }
+
+export default routes;
